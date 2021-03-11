f03a6d70ed0d8fb5b2b258098431108b713d5068
Jared de Uriarte <jared.deuri@gmail.com>
2021-03-11 18:32:28 -0500

    trying to get timer to work

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 var startScreen = document.getElementById("start-screen");
 var startButton = document.getElementById("start-button");
 var questionContainer = document.createElement("div")
+
 var questions = [
 
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["Strings", "Booleans", "Alerts", "Numbers"],
         correctAnswer: "Alerts",
-        
+
     },
 
     {
@@ -39,39 +40,61 @@ var questions = [
     }
 ]
 
-var currentQuestion = 0 
+var currentQuestion = 0
+var currentChoices = 0
 
-// WHEN I click the start button ✅
-// THEN a timer starts ❌ and I am presented with a question ✅
 
-// QUESTION SCREEN AND BUTTONS SHOULD APPEAR
-function startQuestions(){
+
+
+
+//TIMER
+
+document.addEventListener('DOMContentLoaded', () => {
+    var timerDisplay = document.querySelector("#time-left")
+    timeLeft = 60
+    function countDown(){
+        setInterval(function(){
+        if(timeLeft <= 0){
+            clearInterval(timeLeft = 0)
+        }
+        timerDisplay.innerHTML = timeLeft 
+        timeLeft -= 1
+        }, 1000)
+    }
+})
+
+
+function startQuestions() {
     var button = document.createElement("button")
     button.textContent = "Next Question";
     button.addEventListener("click", nextQuestion);
-    questionContainer.textContent = questions [currentQuestion].question;
+    questionContainer.textContent = questions[currentQuestion].question;
     document.body.appendChild(questionContainer);
     document.body.appendChild(button);
+
 }
 
-function nextQuestion(){
+function nextQuestion() {
     currentQuestion++;
-    questionContainer.textContent = questions [currentQuestion].question;
+    questionContainer.textContent = questions[currentQuestion].question;
 
 }
 
 
-startButton.addEventListener("click", function() {
-    // console.log("CLICKED!");
+startButton.addEventListener("click", function () {
     startScreen.setAttribute("style", "display:none;")
     startQuestions();
 
+
+
+
+    
 })
 
 
 
-
-
+// WHEN I click the start button ✅
+// THEN a timer starts ❌ and I am presented with a question ✅
 
 // WHEN I answer a question
 // THEN I am presented with another question
@@ -84,3 +107,9 @@ startButton.addEventListener("click", function() {
 
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+
+
+
+//Timer
+
