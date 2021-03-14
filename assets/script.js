b88bef5b15d7e8a36a3730129fa38a0f6563edbc
Jared de Uriarte <jared.deuri@gmail.com>
2021-03-13 20:14:24 -0500

    choices iterated and choices buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 var startScreen = document.getElementById("start-screen");
 var startButton = document.getElementById("start-button");
 var questionContainer = document.createElement("div")
+var answerContainer = document.createElement("div")
 
-var questions = [
+
+// This is my list of questions, choices of 
+var questionsList = [
 
     {
         question: "Commonly used data types DO NOT include:",
@@ -40,56 +43,88 @@ var questions = [
     }
 ]
 
-var currentQuestion = 0
+var currentQuestionIndex = 0
 var currentChoices = 0
 
 
 
-
-
-//TIMER
-
-document.addEventListener('DOMContentLoaded', () => {
-    var timerDisplay = document.querySelector("#time-left")
-    timeLeft = 60
-    function countDown(){
-        setInterval(function(){
-        if(timeLeft <= 0){
-            clearInterval(timeLeft = 0)
-        }
-        timerDisplay.innerHTML = timeLeft 
-        timeLeft -= 1
-        }, 1000)
-    }
-})
-
-
 function startQuestions() {
+    // creating a new button called "next question"
     var button = document.createElement("button")
+    // going into the new button and making the "label"
     button.textContent = "Next Question";
+    // Adding event listening to the new button to call the funtion nextQuestion
     button.addEventListener("click", nextQuestion);
-    questionContainer.textContent = questions[currentQuestion].question;
+    // Adding the question texts to the div, which i created in the DOM
+    questionContainer.textContent = questionsList[currentQuestionIndex].question;
+    console.log(questionsList[currentQuestionIndex])
     document.body.appendChild(questionContainer);
     document.body.appendChild(button);
+    //Looping through all choices for the currentQuestionIndex
+    answerContainer.innerHTML = "";
+    for (var i = 0; i < 4; i++) {
+        questionsList[currentQuestionIndex].choices[i]
+        console.log(questionsList[currentQuestionIndex].choices[i])
+        var button = document.createElement("button")
+        button.textContent = questionsList[currentQuestionIndex].choices[i];
+        button.addEventListener("click", checkAnswer);
+        // adding buttons to the answer container
+        answerContainer.appendChild(button);
+        // adding the answer container to the DOM body
+        document.body.appendChild(answerContainer);
+    }
 
 }
 
 function nextQuestion() {
-    currentQuestion++;
-    questionContainer.textContent = questions[currentQuestion].question;
+    // Incrementing the index by 1 to move to the next question in the array of questionsList. 
+    currentQuestionIndex = currentQuestionIndex + 1;
+    questionContainer.textContent = questionsList[currentQuestionIndex].question;
+    //Looping through all choices for the currentQuestionIndex
+    answerContainer.innerHTML = "";
+    for (var i = 0; i < 4; i++) {
+        questionsList[currentQuestionIndex].choices[i]
+        console.log(questionsList[currentQuestionIndex].choices[i])
+        var button = document.createElement("button")
+        button.textContent = questionsList[currentQuestionIndex].choices[i];
+        button.addEventListener("click", checkAnswer);
+        // adding buttons to the answer container
+        answerContainer.appendChild(button);
+        // adding the answer container to the DOM body
+        document.body.appendChild(answerContainer);
+    }
+    //Populating and looping  answer buttons
+    // questionsList[currentQuestionIndex].choices.forEach(function(choice) {
+    // console.log(choice)
+    // })
 
+    // for(var i = 0; i < questionsList[currentQuestionIndex].choices.length)
+
+}
+
+function checkAnswer() {
+    console.log("checkAnswer");
 }
 
 
 startButton.addEventListener("click", function () {
     startScreen.setAttribute("style", "display:none;")
     startQuestions();
-
-
-
-
-    
 })
+
+
+
+var timerDisplay = document.querySelector("#time-left")
+timeLeft = 60
+setInterval(function () {
+    if (timeLeft <= 0) {
+        clearInterval(timeLeft = 0)
+    }
+    timerDisplay.innerHTML = timeLeft
+    timeLeft -= 1
+}, 1000);
+
+
 
 
 
